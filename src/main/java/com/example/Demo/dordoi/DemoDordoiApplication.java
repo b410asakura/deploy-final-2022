0cd557f739d1930e51afed71db7b392064d08ab5
b410asakura <73512839+b410asakura@users.noreply.github.com>
2022-06-09 14:24:22 +0600

    for deploying

---
+++ src/main/java/com/example/Demo/dordoi/DemoDordoiApplication.java
@@ -0,0 +1,13 @@
+package com.example.Demo.dordoi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDordoiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoDordoiApplication.class, args);
+	}
+
+}
